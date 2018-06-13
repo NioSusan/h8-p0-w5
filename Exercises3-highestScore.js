7266fa42d9335572f7c8bf7cb9a4a9efe1234ea3
Susan Nio <susan.wen88@gmail.com>
2018-06-13 13:40:37 +0700

    Updating Exercises 3

--- Exercises3-highestScore.js
+++ Exercises3-highestScore.js
@@ -4,54 +4,54 @@ function sortObj(obj1, obj2){
 }
 
 function highestScore (students) {
-var obj={};
+    var obj={};
 
-if(students.length===0){
-return obj;
-}
+    if(students.length===0){
+    return obj;
+    }
 
-students.sort(sortObj);//call the function to sort the score
-// console.log(students);
+    students.sort(sortObj);//call the function to sort the score
+    // console.log(students);
 
-var currentHighestScore;
-var currentHighestScoreClass;
-var lowGroup=[];
-var highGroup=[];
+    var currentHighestScore;
+    var currentHighestScoreClass;
+    var lowGroup=[];
+    var highGroup=[];
 
-for(var i=0;i<students.length;i++){
-currentHighestScore = students[students.length-1].score;//e.g. 100
-currentHighestScoreClass = students[students.length-1].class//e.g. foxes
-//if same class but lower score, push to lowGroup
-if(students[i].class === currentHighestScoreClass && students[i].score < currentHighestScore){
-lowGroup.push(students[i]);
-//if same class and same score to currentHighestScore & currentHighestScoreClass, add to obj
-} else if (students[i].class === currentHighestScoreClass && students[i].score === currentHighestScore){
-obj[students[i].class]=students[i];
-//if different class, push to highGroup
-} else if (students[i].class !== currentHighestScoreClass){
-if(highGroup[0] === undefined){
-highGroup.push(students[i]);
-} else if (students[i].class ===highGroup[0].class &&students[i].score > highGroup[0].score){
-highGroup[0] = students[i];
-} else if (students[i].class !==highGroup[0].class && highGroup[1]===undefined){
-highGroup.push(students[i]);
-} else if (students[i].class ===highGroup[1].class &&students[i].score > highGroup[1].score){
-highGroup[1] = students[i];
-}
-}
-}
+    for(var i=0;i<students.length;i++){
+        currentHighestScore = students[students.length-1].score;//e.g. 100
+        currentHighestScoreClass = students[students.length-1].class//e.g. foxes
+        //if same class but lower score, push to lowGroup
+        if(students[i].class === currentHighestScoreClass && students[i].score < currentHighestScore){
+            lowGroup.push(students[i]);
+        //if same class and same score to currentHighestScore & currentHighestScoreClass, add to obj
+        } else if (students[i].class === currentHighestScoreClass && students[i].score === currentHighestScore){
+            obj[students[i].class]=students[i];
+        //if different class, push to highGroup
+        } else if (students[i].class !== currentHighestScoreClass){
+            if(highGroup[0] === undefined){
+                highGroup.push(students[i]);
+            } else if (students[i].class ===highGroup[0].class &&students[i].score > highGroup[0].score){
+                highGroup[0] = students[i];
+            } else if (students[i].class !==highGroup[0].class && highGroup[1]===undefined){
+                highGroup.push(students[i]);
+            } else if (students[i].class ===highGroup[1].class &&students[i].score > highGroup[1].score){
+                highGroup[1] = students[i];
+            }
+        }
+    }
 
-//add each elem of highGroup to obj
-for(var elem of highGroup){
-obj[elem.class] = elem;
-}
+    //add each elem of highGroup to obj
+    for(var elem of highGroup){
+        obj[elem.class] = elem;
+    }
 
-//remove value of class from every key(such as foxes,wolves,tigers) of obj
-for(var key in obj){
-// console.log(obj[key]);
-delete obj[key].class;
-}
-return obj;
+    //remove value of class from every key(such as foxes,wolves,tigers) of obj
+    for(var key in obj){
+    // console.log(obj[key]);
+        delete obj[key].class;
+    }
+    return obj;
 }
 
 // TEST CASE
